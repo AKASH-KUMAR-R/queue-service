d03448b2bf37b0ec057fcd6bd07a10ad4e1ec3b2
AKASH-KUMAR-R <akashkumar77464@gmail.com>
2025-12-07 03:55:53 +0530

    feat: queue routes validations added

--- api/src/routes/queue/queue.routes.ts
+++ api/src/routes/queue/queue.routes.ts
@@ -1,15 +1,31 @@
 import { Router } from "express";
+
 import commonController from "@common/controller/common.controller";
+import {
+	validateId,
+	validationMiddleware,
+} from "@common/middleware/zod.middleware";
+import { QueueCreateRequest } from "@models/queue/requests/QueueCreateRequest";
+import { QueueUpdateRequest } from "@models/queue/requests/QueueUpdateRequest";
 
 const router = Router();
 
 router.get("/list", commonController.list);
 router.get("/search", commonController.search);
-router.get("/:id", commonController.getById);
+router.get("/:id", validateId, commonController.getById);
 
-router.post("/create", commonController.upsert);
-router.put("/:id", commonController.update);
+router.post(
+	"/create",
+	validationMiddleware(QueueCreateRequest),
+	commonController.upsert
+);
+router.put(
+	"/:id",
+	validateId,
+	validationMiddleware(QueueUpdateRequest),
+	commonController.update
+);
 
-router.delete("/:id", commonController.remove);
+router.delete("/:id", validateId, commonController.remove);
 
 export default router;
