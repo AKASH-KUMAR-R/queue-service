bd5c440ea3f9e8349eebc42bccc61e5029cbca9f
AKASH-KUMAR-R <akashkumar77464@gmail.com>
2025-12-06 20:42:14 +0530

    feat: queue routes added

--- api/src/routes/queue/queue.routes.ts
+++ api/src/routes/queue/queue.routes.ts
@@ -1,7 +1,15 @@
 import { Router } from "express";
-import { addQueue } from "../../controllers/queue/queue.controller";
+import commonController from "../../common/controller/common.controller";
+
 const router = Router();
 
-router.post("/create", addQueue);
+router.get("/:id", commonController.getById);
+router.get("/list", commonController.list);
+router.get("/search", commonController.search);
+
+router.post("/create", commonController.upsert);
+router.put("/:id", commonController.upsert);
+
+router.delete("/:id", commonController.remove);
 
 export default router;
