c4a5d8ecbf4ee52011ab9579261d3a4b5427ef91
AKASH-KUMAR-R <akashkumar77464@gmail.com>
2025-12-07 15:02:21 +0530

    feat: jobs model added and resolved some db migration issues

---
+++ api/scripts/dev/modifyExistingStatus.js
@@ -0,0 +1,28 @@
+
+import { PrismaClient } from "@prisma/client";
+
+
+const prisma = new PrismaClient({});
+
+
+const modifyExistingStatus = async () => {
+    try {
+
+        const result = await prisma.queue.updateMany({
+            where: {
+                status: "QUEUED",
+            },
+            data: {
+                status: "ACTIVE",
+            }
+        });
+
+        console.log(`Modified ${result.count} records from QUEUED to ACTIVE.`);
+    } catch (error) {
+        console.error("Error modifying existing status:", error);
+    } finally {
+        await prisma.$disconnect();
+    }
+};
+
+modifyExistingStatus();
