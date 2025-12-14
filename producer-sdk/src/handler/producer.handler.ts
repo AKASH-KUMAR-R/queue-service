f056b3b7122e8a2e695273074a39f20a7d481efc
= <=>
2025-12-14 23:04:53 +0530

    feat: producer new fields added

--- producer-sdk/src/handler/producer.handler.ts
+++ producer-sdk/src/handler/producer.handler.ts
@@ -4,27 +4,27 @@ import { AddJobOptions, ProducerOptions } from "../types/producer";
 import handleError from "../utils/error.util";
 
 export default function createProducer(options: ProducerOptions) {
-	const client = getApiClient({
-		apiKey: options.apiKey,
-		baseURL: options.baseUrl,
-	});
+    const client = getApiClient({
+        apiKey: options.apiKey,
+        baseURL: options.baseUrl,
+    });
 
-	async function addJob(queueLabel: string, options: AddJobOptions) {
-		try {
-			const res = await client.post("/api/worker/job/create", {
-				payload: options.payload,
-				queue_label: queueLabel,
-			});
+    async function addJob(queueLabel: string, options: AddJobOptions) {
+        try {
+            const res = await client.post("/api/worker/job/create", {
+                ...options,
+                queue_label: queueLabel,
+            });
 
-			logger.info(`Job added to ${queueLabel}`);
-			return res.data;
-		} catch (err) {
-			handleError(err);
-			throw err;
-		}
-	}
+            logger.info(`Job added to ${queueLabel}`);
+            return res.data;
+        } catch (err) {
+            handleError(err);
+            throw err;
+        }
+    }
 
-	return {
-		addJob,
-	};
+    return {
+        addJob,
+    };
 }
