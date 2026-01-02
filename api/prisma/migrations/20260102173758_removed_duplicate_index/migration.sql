859a20e252b54717f62c02a16a6b9e462b60ccc5
Akash Kumar R <akashkumar77464@gmail.com>
2026-01-02 23:08:12 +0530

    fix: migration applied to dev db

---
+++ api/prisma/migrations/20260102173758_removed_duplicate_index/migration.sql
@@ -0,0 +1,11 @@
+-- DropIndex
+DROP INDEX "Queue_label_idx";
+
+-- DropIndex
+DROP INDEX "QueueMetrics_queue_id_idx";
+
+-- DropIndex
+DROP INDEX "QueueRateLimiter_queue_id_idx";
+
+-- DropIndex
+DROP INDEX "User_email_idx";
